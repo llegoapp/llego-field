d6e5a4a9b23aebc6406ab89394946c637f4a4db9
Abraxas-365 <luisfmiranda8@gmail.com>
2024-01-04 14:45:17 -0500

    fix(reservation): fix signarute

--- internal/reservation/ports.go
+++ internal/reservation/ports.go
@@ -3,7 +3,7 @@ package reservation
 type ReservationRepository interface {
 	CreateReservation(r Reservation) error
 	GetReservation(id int) (Reservation, error)
-	UpdateReservation(id Reservation) error
+	UpdateReservation(r Reservation) error
 	ListReservation(page, pageSize int) ([]*Reservation, int, error)
 	ListReservationByBookerId(bookerId int, page, pageSize int) ([]*Reservation, int, error)
 	ListReservationByFieldId(fieldId int, page, pageSize int) ([]*Reservation, int, error)
