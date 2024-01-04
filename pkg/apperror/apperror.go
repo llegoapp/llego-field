8b10cf3e6739cd15eedecea79aa96688724eef5a
Abraxas-365 <luisfmiranda8@gmail.com>
2024-01-03 21:30:59 -0500

    feat(error): error type and middleware to work with fiber

---
+++ pkg/apperror/apperror.go
@@ -0,0 +1,38 @@
+package apperror
+
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+type AppError struct {
+	Code    int
+	Message string
+}
+
+func (e *AppError) Error() string {
+	return fmt.Sprintf("Error %d: %s", e.Code, e.Message)
+}
+
+func NewAppError(code int, message string) *AppError {
+	return &AppError{
+		Code:    code,
+		Message: message,
+	}
+}
+
+// ErrorHandler is a middleware that converts AppError to fiber.Error.
+func ErrorHandler(ctx *fiber.Ctx, err error) error {
+	// Convert error to fiber.Error
+	var fiberErr *fiber.Error
+	if e, ok := err.(*AppError); ok {
+		fiberErr = fiber.NewError(e.Code, e.Message)
+	} else {
+		// Default to 500 internal server error
+		fiberErr = fiber.NewError(fiber.StatusInternalServerError, err.Error())
+	}
+
+	// Send the error response
+	return ctx.Status(fiberErr.Code).SendString(fiberErr.Message)
+}
